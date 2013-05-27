ef9701865f89bd40ad590d0d25831f0f2ddbe80f
cavinsb <benjamin.cavins@colorado.edu>
2013-05-27 14:12:19 -0600

    Added urls for resume. Added a view for resume.

--- resume/views.py
+++ resume/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from resume.models import Resume
+
+def index(request):
+    
+    # Grab the resume created last
+    resume = Resume.objects.order_by('-create_datetime')[0]
+    output = 'This resume is for: '+resume.get_full_name()
+    return HttpResponse(output)
