27932b27181a61ee5320228ab905bea7a2b45be7
cavinsb <benjamin.cavins@colorado.edu>
2013-05-27 14:24:30 -0600

    Created index.html for resume. Updated resume/views.py to load this file.

--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 
 from resume.models import Resume
 
@@ -6,5 +7,6 @@ def index(request):
     
     # Grab the resume created last
     resume = Resume.objects.order_by('-create_datetime')[0]
-    output = 'This resume is for: '+resume.get_full_name()
-    return HttpResponse(output)
+    template = loader.get_template('resume/index.html')
+    context = Context({'resume': resume})
+    return HttpResponse(template.render(context))
